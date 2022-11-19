645a90f3f88adf9fa9a63d74c2009071cab6f441
Alexander Popov <alex.wayfer@gmail.com>
2022-11-19 17:27:52 +0300

    Add Codecov and SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,15 @@
 require 'faraday'
 require 'faraday/encode_xml'
 
+require 'simplecov'
+
+if ENV['CI']
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+end
+
+SimpleCov.start
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
